a8a446c5c9758d54ed4539a8e095364e5acf46cb
kodlan <6258615+kodlan@users.noreply.github.com>
2022-12-26 18:44:21 +0200

    Added sample achievement update functionality

--- microservices/achievementservice/src/main/java/k/ketchapp/service/achievementservice/dao/AchievementDao.java
+++ microservices/achievementservice/src/main/java/k/ketchapp/service/achievementservice/dao/AchievementDao.java
@@ -1,54 +1,87 @@
 package k.ketchapp.service.achievementservice.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import k.ketchapp.proto.Achievement;
 import k.ketchapp.proto.AchievementProgress;
 import k.ketchapp.proto.Event;
 
 public class AchievementDao {
 
+  private final List<Achievement> achievements = new ArrayList<>();
+
+  private final Map<Integer, AchievementProgress> achievementProgressMap = new HashMap<>();
+
+  public AchievementDao() {
+    initialize();
+  }
+
   public List<Achievement> getAchievements() {
-    return List.of(
-        Achievement.newBuilder()
-            .setId(1)
-            .setNameId(1) // "7 days in a row"
-            .build(),
-
-        Achievement.newBuilder()
-            .setId(2)
-            .setNameId(2) // "30 days in a row"
-            .build(),
-
-        Achievement.newBuilder()
-            .setId(3)
-            .setNameId(3) // "Multiple events in a day"
-            .build()
-    );
+    return achievements;
   }
 
   public AchievementProgress getAchievementProgress(Achievement achievement) {
-    return switch (achievement.getId()) {
-      case 1 -> AchievementProgress.newBuilder()
-          .setCompleted(false)
-          .setProgress(60)
-          .build();
-
-      case 2 -> AchievementProgress.newBuilder()
-          .setCompleted(true)
-          .setProgress(100)
-          .build();
-
-      case 3 -> AchievementProgress.newBuilder()
-          .setCompleted(false)
-          .setProgress(0)
-          .build();
-
-      default -> null;
-    };
+    return achievementProgressMap.get(achievement.getId());
   }
 
   public void updateAchievement(Event event) {
-    // TODO: implement something
+    for (Map.Entry<Integer, AchievementProgress> entry : achievementProgressMap.entrySet()) {
+
+      AchievementProgress oldProgress = entry.getValue();
+
+      if (!oldProgress.getCompleted()) {
+
+        int newProgressValue = oldProgress.getProgress() + 1;
+        boolean isCompleted = oldProgress.getCompleted() || newProgressValue >= 100;
+
+        AchievementProgress newProgress = AchievementProgress.newBuilder(oldProgress)
+            .setProgress(newProgressValue)
+            .setCompleted(isCompleted)
+            .build();
+
+        entry.setValue(newProgress);
+      }
+    }
   }
 
+  private void initialize() {
+    achievements.addAll(
+        List.of(
+            Achievement.newBuilder()
+                .setId(1)
+                .setNameId(1) // "7 days in a row"
+                .build(),
+
+            Achievement.newBuilder()
+                .setId(2)
+                .setNameId(2) // "30 days in a row"
+                .build(),
+
+            Achievement.newBuilder()
+                .setId(3)
+                .setNameId(3) // "Multiple events in a day"
+                .build()
+        )
+    );
+
+    achievementProgressMap.putAll(
+        Map.of(1, AchievementProgress.newBuilder()
+                .setCompleted(false)
+                .setProgress(60)
+                .build(),
+
+            2, AchievementProgress.newBuilder()
+                .setCompleted(true)
+                .setProgress(100)
+                .build(),
+
+            3, AchievementProgress.newBuilder()
+                .setCompleted(false)
+                .setProgress(0)
+                .build()
+        )
+    );
+  }
 }
